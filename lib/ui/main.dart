7cb66974eefda93542f6e588c1672f4655ba8914
giovannijunseokim <kingjunseo@gmail.com>
2024-06-14 09:22:38 +0900

    6/12

--- lib/ui/main.dart
+++ lib/ui/main.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:simple_todo_list/domain/models/todo.dart';
 import 'package:simple_todo_list/ui/constants/todo_colors.dart';
 import 'package:simple_todo_list/ui/constants/todo_fonts.dart';
 import 'package:simple_todo_list/ui/screens/home_screen.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter(TodoAdapter());
+  await Hive.openBox<Todo>('todoList');
+  var box = Hive.box<Todo>('todoList');
+
+  box.add(Todo(
+    id: DateTime.now().toString(),
+    todoContent: 'Hive 연습하기',
+  ));
+
+  box.toMap().forEach((key, value) {
+    print(
+      '[key] $key, [value] id: ${value.id}| todoContent: ${value.todoContent}| isDone: ${value.isDone}',
+    );
+  });
+
   runApp(const MyApp());
 }
 
